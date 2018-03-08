dbafe8dd2ccc9274878711e957e4ab4fdba52681
郑良凯 <liangkai.zheng@datayes.com>
2018-03-08 19:40:49 +0800

    add Schem in WXApp

--- MyApp/MyApp/ViewController.m
+++ MyApp/MyApp/ViewController.m
@@ -14,15 +14,22 @@
 
 @implementation ViewController
 
+#pragma mark - View Life Cycle
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+#pragma mark - Actions
+- (IBAction)openWXApp:(UIButton *)sender {
+	[self demo1];
+}
+- (void)demo1 {
+	//创建一个url，这个url就是WXApp的url，记得加上：//
+	NSURL *url = [NSURL URLWithString:@"weixin://"];
+	
+	//打开url
+	[[UIApplication sharedApplication] openURL:url];
 }
 
 
